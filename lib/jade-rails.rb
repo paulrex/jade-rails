6c7c6009fd8c932bb19afd769910e114fed8b755
Paul Raythattha <paul@appfactories.com>
2014-11-27 20:44:54 -0800

    Wrap jade.compileClient using ExecJS.

---
+++ lib/jade-rails.rb
@@ -0,0 +1,20 @@
+# Jade Template Compiler for Ruby
+
+require 'json'
+require 'execjs'
+
+module Jade
+  class << self
+
+    def compile(source, options = {})
+      jade_js = File.read(File.expand_path('../../vendor/assets/javascripts/jade/jade.js', __FILE__))
+      context = ExecJS.compile <<-JS
+        var window = {};
+        #{jade_js}
+        var jade = window.jade;
+      JS
+      context.eval("jade.compileClient(#{source.to_json}, #{options.to_json})")
+    end
+
+  end
+end
