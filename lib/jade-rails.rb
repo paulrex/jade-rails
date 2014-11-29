4cd37689ec74fab64e8f9ec829936e6418525d74
Paul Raythattha <paul@appfactories.com>
2014-11-28 21:00:47 -0800

    Jade.compile: Improve performance by memoizing context.

--- lib/jade-rails.rb
+++ lib/jade-rails.rb
@@ -7,14 +7,16 @@ module Jade
   class << self
 
     def compile(source, options = {})
-      jade_js = File.read(File.expand_path('../../vendor/assets/javascripts/jade/jade.js', __FILE__))
-      context = ExecJS.compile <<-JS
-        var window = {};
-        #{jade_js}
-        var jade = window.jade;
-      JS
+      @@context ||= begin
+        jade_js = File.read(File.expand_path('../../vendor/assets/javascripts/jade/jade.js', __FILE__))
+        ExecJS.compile <<-JS
+          var window = {};
+          #{jade_js}
+          var jade = window.jade;
+        JS
+      end
       source = source.read if source.respond_to?(:read)
-      context.eval("jade.compileClient(#{source.to_json}, #{options.to_json})")
+      @@context.eval("jade.compileClient(#{source.to_json}, #{options.to_json})")
     end
 
   end
