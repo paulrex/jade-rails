957fdf47f5cd5ece17c001b8da747e1d8b868970
Paul Raythattha <paul@appfactories.com>
2016-06-03 18:30:54 -0700

    Integration Testing: Set up functionality for instantiating Rails app.

--- integration-tests/run-tests.sh
+++ integration-tests/run-tests.sh
@@ -12,10 +12,60 @@
 #             - the application.js file compiles
 #             - the application.js file contains the Jade template JS
 #
+# ASSUMPTIONS:
+#   - This script is running on OS X. (The sed syntax is specific to OS X.)
+#   - The bundler gem is globally available.
+#   - rbenv is being used to manage Ruby versions.
 #---------------------------------------------------------------------------------------------------
 set -e
 set -o pipefail
 
-rails_versions=(4.1.15 4.2.6) 
-echo ${rails_versions[0]}
-echo ${rails_versions[1]}
+# To simplify all the other references to files and paths, force this script to only run from inside
+# the integration-tests directory itself.
+current_directory=$(pwd)
+if [[ $current_directory != *"integration-tests" ]]
+then
+  echo
+  echo "ERROR"
+  echo "This script must be run from inside the integration-tests directory."
+  echo
+  exit 1
+fi
+
+# Test against the currently-supported Rails versions.
+# See: http://guides.rubyonrails.org/maintenance_policy.html
+# rails_versions=(4.1.15 4.2.6)
+rails_versions=(4.1.15)
+dev_server_port=30000
+for rails_version in ${rails_versions[@]}; do
+  echo
+  echo "Beginning integration test for Rails v${rails_version}..."
+  echo
+
+  # Set up the version of Rails we're testing against.
+  sed -i '' "5 s/.*/gem 'rails', '${rails_version}'/" ./Gemfile
+  bundle install
+  rbenv rehash
+  installed_rails_version=$(rails -v)
+  if [[ $installed_rails_version != "Rails ${rails_version}" ]]
+  then
+    echo
+    echo "ERROR"
+    echo "Failed to correctly install Rails version ${rails_version}."
+    echo
+    exit 1
+  fi
+
+  # Instantiate a new Rails app using that version.
+  app_name="test-${rails_version}"
+  rails new ${app_name}
+
+  # TODO: Add test code here.
+
+  # Clean out the instantiated Rails app.
+  rm -r ${app_name}
+
+  echo
+  echo "Successfully completed integration test for Rails v${rails_version}."
+  echo
+done
